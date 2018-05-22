d130b627926e096dd2c5b4c01ab8b899b54b832b
pavansri <38874825+pavansri@users.noreply.github.com>
2018-05-22 09:53:24 +0530

    Update media.py

--- media.py
+++ media.py
@@ -5,6 +5,13 @@ import webbrowser
 class Movie():
     VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
 
+'''class Movie():
+attributes:
+    movie_title=It describes the title of the movie.
+    movie_storyline=It summarises the story of the movie in one line.
+    poster_image=This is the poster of the respective movie.
+    trailer_youtube=This is the trailer of the repective movie 
+'''
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
