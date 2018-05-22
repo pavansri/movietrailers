cf84fa1f413538b3ac2cd5cbdd1d1dfa1adc244a
pavansri <38874825+pavansri@users.noreply.github.com>
2018-05-22 22:45:10 +0530

    Update media.py

--- media.py
+++ media.py
@@ -3,15 +3,15 @@ import webbrowser
 
 
 class Movie():
-    VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
-
-'''class Movie():
+    '''class Movie():
 attributes:
     movie_title=It describes the title of the movie.
     movie_storyline=It summarises the story of the movie in one line.
     poster_image=This is the poster of the respective movie.
-    trailer_youtube=This is the trailer of the repective movie 
+    trailer_youtube=This is the trailer of the repective movie
 '''
+    VALID_RATINGS = ["EXCELLENT", "GOOD", "BAD", "AVERAGE"]
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -20,6 +20,4 @@ attributes:
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-'''As we imported webbrowser we can open the link with webbrowser.open.
-'''
         webbrowser.open(self.trailer_youtube_url)
